52d9aa7a9a5d7f7c883009e031b31a8e04c8b722
houpai <houpai@qq.com>
2020-12-07 13:40:24 +0800

    commit

---
+++ src/views/demo/demo1.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="demo1-container">
+    demo1
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'demo1'
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/demo/demo2.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="demo2-container">
+    demo2
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'demo2'
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
